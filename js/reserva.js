8a9d699136d8baa963cb26f91181e062d65c7894
juanfranfdezGit <jfranromfdez@gmail.com>
2023-04-01 19:00:17 +0200

    UPDATE
    af

--- js/reserva.js
+++ js/reserva.js
@@ -3,73 +3,61 @@ let meses = [
     {
         nombre: 'Enero',
         dias: ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31"],
-        number:0
     },
 
     {
         nombre: 'Febrero',
         dias: ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28"],
-        number:1
     },
 
     {
         nombre: 'Marzo',
         dias: ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31"],
-        number:2
     },
 
     {
         nombre: 'Abril',
         dias: ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30"],
-        number:3
     },
 
     {
         nombre: 'Mayo',
         dias: ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31"],
-        number:4
     },
 
     {
         nombre: 'Junio',
         dias: ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30"],
-        number:5
     },
 
     {
         nombre: 'Julio',
         dias: ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31"],
-        number:6
     },
 
     {
         nombre: 'Agosto',
         dias: ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31"],
-        number:7
     },
 
     {
         nombre: 'Septiembre',
         dias: ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30"],
-        number:8
     },
 
     {
         nombre: 'Octubre',
         dias: ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31"],
-        number:9
     },
 
     {
         nombre: 'Noviembre',
         dias: ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30"],
-        number:10
     },
 
     {
         nombre: 'Diciembre',
         dias: ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31"],
-        number:11
     }
 ];
 
@@ -80,8 +68,8 @@ const contenedorDias = document.querySelector(".dias");
 
 // CREAR CALENDARIO
 const fecha = new Date();
-let mesHoy = fecha.getMonth() + 1;
-let diaHoy = fecha.getDate() - 1;
+let mesHoy = fecha.getMonth();
+let diaHoy = fecha.getDate();
 
 for(i = 1; i < meses.length; i++){
         // ESCRIBE MES ACTUAL Y RECOGE LOS DIAS SEGUN DATOS EN JSON
@@ -94,6 +82,9 @@ for(i = 1; i < meses.length; i++){
             let dia = document.createElement("p");
             dia.innerHTML = diasCont[i];
             contenedorDias.appendChild(dia);
+            if(diasCont[i] == diaHoy){
+                dia.classList.add("diaHoy")
+            }
         }
     }
 
@@ -118,7 +109,7 @@ startDate.addEventListener("click", function(){
     calendario.classList.add("Act")
 })
 
-// CERRAR FORMULARIO
+// CERRAR FORMULARIO Y CALENDARIO
 const cerrar = document.querySelector(".cerrar")
 
 cerrar.addEventListener("click", () => {
@@ -132,18 +123,22 @@ cerrar.addEventListener("click", () => {
 const posterior = document.querySelector(".pos");
 const anterior = document.querySelector(".ant");
 const diasCreated = document.querySelectorAll(".dias p");
+const reservado = document.querySelector(".reservado")
 
 anterior.addEventListener("click", () => {
     for(i = 1; i < meses.length; i++){
+
         // ESCRIBE MES ACTUAL Y RECOGE LOS DIAS SEGUN DATOS EN JSON
-        mesHoy--;
         let mes = meses[mesHoy]
-        let diasCont = mes.dias
-        mesActual.innerHTML = mes.nombre;
+        let diasContDel = mes.dias
+        mesHoy--;
+        let mesProx = meses[mesHoy]
+        let diasCont = mesProx.dias
+        mesActual.innerHTML = mesProx.nombre;
 
         //BORRAR DIAS ANTERIORES
-        for(i = 0; i < diasCont.length; i++){
-           let dia =document.querySelector(".dias p");
+        for(i = 0; i < diasContDel.length; i++){
+           let dia = document.querySelector(".dias p");
             contenedorDias.removeChild(dia);
         }
 
@@ -152,6 +147,30 @@ anterior.addEventListener("click", () => {
             let dia = document.createElement("p");
             dia.innerHTML = diasCont[i];
             contenedorDias.appendChild(dia);
+
+            // REGISTRAMOS LOS NUEVOS DIAS CREADOS EN CADA MES PARA PODER CLICKARLOS
+            let newDays = document.querySelectorAll(".dias p")
+
+            newDays.forEach(day => {
+                day.addEventListener("click", () => {
+                    reservado.classList.add("activarRes")
+        
+                    form.classList.remove("form-activ")
+                    tituloform.classList.remove("formtitnone");
+                    formul.classList.remove("formtext-none");
+                    calendario.classList.remove("Act")
+                })
+             })
+        }
+
+        //CONDICIONAL PARA IR MAS ATRAS DE ENERO
+        if(mesHoy == 0){
+            anterior.style = "display:none"
+            console.log("HOLA")
+        }else{
+            anterior.style = "display:block"
+            posterior.style = "display:block"
+            console.log("ADIOS")
         }
     }
 })
@@ -159,15 +178,17 @@ anterior.addEventListener("click", () => {
 posterior.addEventListener("click", () => {
     for(i = 1; i < meses.length; i++){
         // ESCRIBE MES ACTUAL Y RECOGE LOS DIAS SEGUN DATOS EN JSON
-        mesHoy++;
         let mes = meses[mesHoy]
-        let diasCont = mes.dias
-        mesActual.innerHTML = mes.nombre;
-        console.log(mes.dias)
+        let diasContDel = mes.dias
+        mesHoy++;
+        let mesProx = meses[mesHoy]
+        let diasCont = mesProx.dias
+        console.log(mesProx)
+        mesActual.innerHTML = mesProx.nombre;
 
         //BORRAR DIAS ANTERIORES
-        for(i = 0; i < diasCont.length; i++){
-            let dia =document.querySelector(".dias p");
+        for(i = 0; i < diasContDel.length; i++){
+            let dia = document.querySelector(".dias p");
             contenedorDias.removeChild(dia);
         }
 
@@ -176,23 +197,44 @@ posterior.addEventListener("click", () => {
             let dia = document.createElement("p");
             dia.innerHTML = diasCont[i];
             contenedorDias.appendChild(dia);
-            console.log(dia)
+
+             // REGISTRAMOS LOS NUEVOS DIAS CREADOS EN CADA MES PARA PODER CLICKARLOS
+            let newDays = document.querySelectorAll(".dias p")
+
+            newDays.forEach(day => {
+                day.addEventListener("click", () => {
+                    reservado.classList.add("activarRes")
+        
+                    form.classList.remove("form-activ")
+                    tituloform.classList.remove("formtitnone");
+                    formul.classList.remove("formtext-none");
+                    calendario.classList.remove("Act")
+                })
+             })
+        }
+
+        // CONDICIONAL PARA NO SEGUIR DESPUES DE DICIEMBRE
+        if(mesHoy == 11){
+            posterior.style = "display:none"
+            console.log("HOLA")
+        }else{
+            posterior.style = "display:block"
+            anterior.style = "display:block"
         }
     }
 })
 
 // FINALIZAR RESERVA
-const reservado = document.querySelector(".reservado")
 
 diasCreated.forEach(day => {
         day.addEventListener("click", () => {
-        reservado.classList.add("activarRes")
+            reservado.classList.add("activarRes")
 
-        form.classList.remove("form-activ")
-        tituloform.classList.remove("formtitnone");
-        formul.classList.remove("formtext-none");
-        calendario.classList.remove("Act")
-    })
+            form.classList.remove("form-activ")
+            tituloform.classList.remove("formtitnone");
+            formul.classList.remove("formtext-none");
+            calendario.classList.remove("Act")
+        })
 })
 
 // CERRAR MENSAJE
